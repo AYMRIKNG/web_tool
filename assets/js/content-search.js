67305d61217969ceac1e23dc813ab8f53497bfd8
Aymerick <120599499+AYMRIKNG@users.noreply.github.com>
2024-12-08 02:07:33 +0100

    fin partie recrutement

--- assets/js/content-search.js
+++ assets/js/content-search.js
@@ -1,35 +1,42 @@
-//关键词sug
 var hotList = 0;
+
 $(function() {
-    //当键盘键被松开时发送Ajax获取数据
+    // Lorsqu'une touche est relâchée dans le champ de recherche
     $('#search-text').keyup(function() {
-        var keywords = $(this).val();
-        if (keywords == '') { $('#word').hide(); return };
+        var keywords = $(this).val();  // Récupérer la valeur du champ de recherche
+        if (keywords == '') { 
+            $('#word').hide();  // Cacher la liste si aucun mot-clé
+            return;
+        }
+
+        // Appel AJAX à l'API Google Suggest
         $.ajax({
-            url: 'https://suggestion.baidu.com/su?wd=' + keywords,
-            dataType: 'jsonp',
-            jsonp: 'cb', //回调函数的参数名(键值)key
-            // jsonpCallback: 'fun', //回调函数名(值) value
-            beforeSend: function() {
-                // $('#word').append('<li>正在加载。。。</li>');
+            url: 'https://suggestqueries.google.com/complete/search?client=firefox&callback=?',  // URL de l'API Google Suggest
+            dataType: 'jsonp',  // Utilisation du format JSONP
+            jsonp: 'callback',  // Le paramètre de callback attendu par l'API
+            data: {
+                q: keywords,  // Mot-clé pour lequel on souhaite des suggestions
+                hl: 'fr',  // Langue des résultats
+                gl: 'fr',  // Région (France)
+                lr: 'lang_fr'
             },
             success: function(res) {
-                $('#word').empty().show();
-                hotList = res.s.length;
+                $('#word').empty().show();  // Vider la liste existante et la rendre visible
+                hotList = res[1].length;  // Nombre de suggestions retournées
                 if (hotList) {
-                    $("#word").css("display", "block");
-                    for (var i = 0; i < hotList-1; i++) {
-                        if (i===hotList-1){
-                            $("#word").append('<li id="lastHot"><span>' + (i + 1) + "</span>" + res.s[i] + "</li>");
-                        }
-                        else{
-                            $("#word").append("<li><span>" + (i + 1) + "</span>" + res.s[i] + "</li>");
-                        }
+                    $("#word").css("display", "block");  // Afficher la liste des suggestions
+                    for (var i = 0; i < hotList; i++) {
+                        // Ajouter chaque suggestion à la liste
+                        $("#word").append("<li><span>" + res[1][i] + "</span></li>");
+
+                        // Ajouter un événement au clic sur chaque suggestion
                         $("#word li").eq(i).click(function() {
-                            $('#search-text').val(this.childNodes[1].nodeValue);
-                            window.open(thisSearch + this.childNodes[1].nodeValue);
-                            $('#word').css('display', 'none')
+                            $('#search-text').val($(this).text());  // Mettre la suggestion dans le champ de recherche
+                            $('#word').css('display', 'none');  // Cacher la liste des suggestions
+                            $('.submit').trigger('click');  // Soumettre le formulaire ou effectuer la recherche
                         });
+
+                        // Ajouter des styles spécifiques aux premières suggestions
                         if (i === 0) {
                             $("#word ul li").eq(i).css({
                                 "border-top": "none"
@@ -37,54 +44,42 @@ $(function() {
                             $("#word ul span").eq(i).css({
                                 "color": "#fff",
                                 "background": "#f54545"
-                            })
+                            });
                         } else if (i === 1) {
                             $("#word ul span").eq(i).css({
                                 "color": "#fff",
                                 "background": "#ff8547"
-                            })
+                            });
                         } else if (i === 2) {
                             $("#word ul span").eq(i).css({
                                 "color": "#fff",
                                 "background": "#ffac38"
-                            })
+                            });
                         }
-                    } 
+                    }
                 } else {
-                        $("#word").css("display", "none")
+                    $("#word").css("display", "none");  // Cacher la liste si aucune suggestion
                 }
-                    
-                /*
-                if (data.s == '') {
-                    //$('#word').append('<div class="error">Not find  "' + keywords + '"</div>');
-                    $('#word').empty();
-                    $('#word').hide();
-                }
-                $.each(data.s, function() {
-                    $('#word').append('<li>' + this + '</li>');
-                })*/
             },
             error: function() {
                 $('#word').empty().show();
-                //$('#word').append('<div class="click_work">Fail "' + keywords + '"</div>');
-                $('#word').hide();
+                $('#word').hide();  // Cacher la liste en cas d'erreur
             }
-        })
-    })
+        });
+    });
 
-    //点击搜索数据复制给搜索框
+    // Lorsqu'un élément de la liste de suggestions est cliqué
     $(document).on('click', '#word li', function() {
-        var word = $(this).text().replace(/^[0-9]/, '');
-        $('#search-text').val(word);
-        $('#word').empty();
-        $('#word').hide();
-        //$("form").submit();
-         $('.submit').trigger('click');//触发搜索事件
-    })
-    //$(document).on('click', '.container,.banner-video,nav', function() {
-    $(document).on('click', '.io-grey-mode', function() {
-        $('#word').empty();
-        $('#word').hide();
-    })
+        var word = $(this).text();  // Récupérer le texte de la suggestion
+        $('#search-text').val(word);  // Remplir le champ de recherche avec la suggestion
+        $('#word').empty();  // Vider la liste
+        $('#word').hide();   // Cacher la liste
+        $('.submit').trigger('click');  // Soumettre le formulaire ou déclencher l'événement de recherche
+    });
 
-})
+    // Cacher la liste de suggestions lorsqu'on clique en dehors
+    $(document).on('click', '.io-grey-mode', function() {
+        $('#word').empty();  // Vider la liste
+        $('#word').hide();   // Cacher la liste
+    });
+});
